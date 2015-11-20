463e93b48aa3c62912d774d5871d35a31c1a6898
elnata93 <natanaelclas@hotmail.com>
2015-11-20 10:24:02 -0400

    Creando clases Atrasos y Prestamos

---
+++ BLL/Atrasos.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class Atrasos : ClaseMaestra
+    {
+        ConexionDb conexion = new ConexionDb();
+
+        public int AtrasosId { get; set; }
+        public int CantidadAtraso { get; set; }
+        
+        public override bool Insertar()
+        {
+            throw new NotImplementedException();
+        }
+        public override bool Editar()
+        {
+            try
+            {
+                bool retorno = false;
+                retorno = conexion.Ejecutar(String.Format("update Atrasos set CantidadAtrasos={0} ", this.CantidadAtraso));
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public override bool Eliminar()
+        {
+            try
+            {
+                bool retorno = false;
+                retorno = conexion.Ejecutar(String.Format("delete from Atrasos where AtrasoId={0} ",this.AtrasosId));
+                return true;
+            }catch(Exception e )
+            {
+                throw e;
+            }
+        }
+
+        public override bool Buscar(int IdBuscado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DataTable Listado(string Campos, string Condicion, string Orden)
+        {
+            string ordenFinal = ""; //!orden.Equals("") ? " orden by  " + orden : "";
+            if (!Orden.Equals(""))
+                ordenFinal = " orden by  " + Orden;
+
+            return conexion.ObtenerDatos(("Select " + Campos + " from Atrasos where " + Condicion + ordenFinal));
+        }
+    }
+}
---
+++ BLL/Prestamos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class Prestamos : ClaseMaestra
+    {
+        ConexionDb conexion = new ConexionDb();
+
+        public override bool Insertar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Editar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Eliminar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Buscar(int IdBuscado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DataTable Listado(string Campos, string Condicion, string Orden)
+        {
+            string ordenFinal = ""; //!orden.Equals("") ? " orden by  " + orden : "";
+            if (!Orden.Equals(""))
+                ordenFinal = " orden by  " + Orden;
+
+            return conexion.ObtenerDatos(("Select " + Campos + " from RutasPrestamos where " + Condicion + ordenFinal));
+        }
+    }
+}
